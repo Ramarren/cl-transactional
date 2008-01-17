284fc9cdb1e2fb0fa65630289c5cb051b5d69db6
Ramarren <ramarren@gmail.com>
2008-01-17 23:55:01 +0100

    Add orelse transactions.

--- package.lisp
+++ package.lisp
@@ -1,6 +1,6 @@
 (defpackage :cl-transactional
   (:nicknames :stm)
   (:use :cl :sb-thread :defclass-star :iterate)
-  (:import-from :arnesi :curry :aif :it :with-unique-names :hash-table-keys))
+  (:import-from :arnesi :curry :aif :it :with-unique-names :hash-table-keys :awhen))
 
 (in-package :cl-transactional)
