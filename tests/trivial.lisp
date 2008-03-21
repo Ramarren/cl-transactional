2b48c170bb8236da126ca2ea584157231a7781fa
Ramarren <ramarren@gmail.com>
2008-03-21 19:44:17 +0100

    Add basic retry-transaction test.

--- tests/trivial.lisp
+++ tests/trivial.lisp
@@ -34,7 +34,7 @@
 		 (list 'a 'b 'c 'd 'e 'f)
 		 :test #'equal))))
 
-;;; Test basic interleaving
+;;; Test basic interleaving, retrying, orelsing
 
 (deftestsuite simple-transactions (cl-transactional-tests)
   (ttvar1 ttvar2)
@@ -82,4 +82,32 @@
       (ensure (queue-empty-p (cl-transactional::value-of ttvar1)))
       (ensure-same (queue-as-list (cl-transactional::value-of ttvar2))
 		   (list 'b 'a 'b 'a 'b 'a 'b 'a 'b 'a)
-		   :test #'equal)))))
+		   :test #'equal)))
+   (retry-and-wait
+    (let ((t1 (make-thread #'(lambda ()
+			       (iter
+				 (repeat 10)
+				 (collect
+				     (with-retry-transaction
+				       (let ((q1 (get-tvar ttvar2)))
+					 (when (queue-empty-p q1)
+					   (retry-transaction))
+					 (put-tvar ttvar2 (dequeue q1))
+					 (queue-first q1))))))
+			   :name "Collector"))
+	  (t2 (make-thread #'(lambda ()
+			       (iter
+				 (let ((q (with-retry-transaction
+					    (let ((q1 (get-tvar ttvar1)))
+					      (unless (queue-empty-p q1)
+						(put-tvar ttvar2 (enqueue (get-tvar ttvar2) (queue-first q1)))
+						(put-tvar ttvar1 (dequeue q1)))))))
+				   (while q)
+				   (thread-yield))))
+			   :name "Mover")))
+      (join-thread t2)
+      (ensure-same (join-thread t1)
+		   (list 'a 'b 'a 'b 'a 'b 'a 'b 'a 'b)
+		   :test #'equal)
+      (ensure (queue-empty-p (cl-transactional::value-of ttvar1)))))))
+
