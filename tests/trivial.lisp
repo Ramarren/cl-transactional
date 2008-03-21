ebb55462c4533ff29041e872bdbe25495e059a7b
Ramarren <ramarren@gmail.com>
2008-03-21 22:59:47 +0100

    Add random test to increase chance of finding an error. As of this commit it hangs due to race condition.

--- tests/trivial.lisp
+++ tests/trivial.lisp
@@ -32,9 +32,17 @@
 		      t)))))))
     (ensure-same (queue-as-list (cl-transactional::value-of ttvar2))
 		 (list 'a 'b 'c 'd 'e 'f)
-		 :test #'equal))))
+		 :test #'equal))
+   (orelse-orelse
+    (ensure-same 'a (with-orelse-transaction
+		      ((retry-transaction))
+		      ('a))))))
 
 ;;; Test basic interleaving, retrying, orelsing
+
+(defrandom-instance random-list simple-transactions
+  (iter (repeat 100)
+	(collect (random-element 'simple-transactions '(a b c d e f g h i j k l m n o p r s)))))
 
 (deftestsuite simple-transactions (cl-transactional-tests)
   (ttvar1 ttvar2)
@@ -84,30 +92,34 @@
 		   (list 'b 'a 'b 'a 'b 'a 'b 'a 'b 'a)
 		   :test #'equal)))
    (retry-and-wait
-    (let ((t1 (make-thread #'(lambda ()
-			       (iter
-				 (repeat 10)
-				 (collect
-				     (with-retry-transaction
-				       (let ((q1 (get-tvar ttvar2)))
-					 (when (queue-empty-p q1)
-					   (retry-transaction))
-					 (put-tvar ttvar2 (dequeue q1))
-					 (queue-first q1))))))
-			   :name "Collector"))
-	  (t2 (make-thread #'(lambda ()
-			       (iter
-				 (let ((q (with-retry-transaction
-					    (let ((q1 (get-tvar ttvar1)))
-					      (unless (queue-empty-p q1)
-						(put-tvar ttvar2 (enqueue (get-tvar ttvar2) (queue-first q1)))
-						(put-tvar ttvar1 (dequeue q1)))))))
-				   (while q)
-				   (thread-yield))))
-			   :name "Mover")))
-      (join-thread t2)
-      (ensure-same (join-thread t1)
-		   (list 'a 'b 'a 'b 'a 'b 'a 'b 'a 'b)
-		   :test #'equal)
-      (ensure (queue-empty-p (cl-transactional::value-of ttvar1)))))))
+    (ensure-random-cases 1000 ((init-list random-list))
+      (setf ttvar1 (make-tvar (make-queue :initial-contents init-list))
+	    ttvar2 (make-tvar (make-queue)))
+      (let ((t1 (make-thread #'(lambda ()
+				 (iter
+				   (repeat 100)
+				   (let ((q (with-retry-transaction
+					      (let ((q1 (get-tvar ttvar2)))
+						(when (queue-empty-p q1)
+						  (retry-transaction))
+						(put-tvar ttvar2 (dequeue q1))
+						(queue-first q1)))))
+				     (collect q))))
+			     :name "Collector"))
+	    (t2 (make-thread #'(lambda ()
+				 (iter
+				   (let ((q (with-retry-transaction
+					      (let ((q1 (get-tvar ttvar1)))
+						(unless (queue-empty-p q1)
+						  (put-tvar ttvar2 (enqueue (get-tvar ttvar2) (queue-first q1)))
+						  (put-tvar ttvar1 (dequeue q1))
+						  (queue-first q1))))))
+				     (while q)
+				     (thread-yield))))
+			     :name "Mover")))
+	(join-thread t2)
+	(ensure-same (join-thread t1) init-list
+		     :test #'equal)
+	(ensure (queue-empty-p (cl-transactional::value-of ttvar1)))
+	(ensure (queue-empty-p (cl-transactional::value-of ttvar2))))))))
 
