7f1434409a6454a33948622145d28db0e6260156
Ramarren <ramarren@gmail.com>
2008-03-21 17:55:49 +0100

    Make trivial test make some sense.

--- tests/trivial.lisp
+++ tests/trivial.lisp
@@ -8,32 +8,28 @@
 	  (setf ttvar2 (make-tvar (make-queue))))
   (:tests
    (retry-transaction
-    (with-retry-transaction
-      (let ((q1 (get-tvar ttvar1))
-	    (q2 (get-tvar ttvar2)))
-       (iterate
-	 (until (queue-empty-p q1))
-	 (let ((q (queue-first q1))
-	       (nq (dequeue q1)))
-	   (setf q1 nq)
-	   (setf q2 (enqueue q2 q))))
-       (put-tvar ttvar1 q1)
-       (put-tvar ttvar2 q2)))
+    (iter
+      (while (with-retry-transaction
+	       (let ((q1 (get-tvar ttvar1))
+		     (q2 (get-tvar ttvar2)))
+		 (unless (queue-empty-p q1)
+		  (let ((q (queue-first q1)))
+		    (put-tvar ttvar1 (dequeue q1))
+		    (put-tvar ttvar2 (enqueue q2 q))
+		    t))))))
     (ensure-same (queue-as-list (cl-transactional::value-of ttvar2))
 		 (list 'a 'b 'c 'd 'e 'f)
 		 :test #'equal))
    (orelse-transaction
-    (with-orelse-transaction
-      ((let ((q1 (get-tvar ttvar1))
-	     (q2 (get-tvar ttvar2)))
-	 (iterate
-	   (until (queue-empty-p q1))
-	   (let ((q (queue-first q1))
-		 (nq (dequeue q1)))
-	     (setf q1 nq)
-	     (setf q2 (enqueue q2 q))))
-	 (put-tvar ttvar1 q1)
-	 (put-tvar ttvar2 q2))))
+    (iter
+      (while (with-orelse-transaction
+	       ((let ((q1 (get-tvar ttvar1))
+		      (q2 (get-tvar ttvar2)))
+		  (unless (queue-empty-p q1)
+		    (let ((q (queue-first q1)))
+		      (put-tvar ttvar1 (dequeue q1))
+		      (put-tvar ttvar2 (enqueue q2 q))
+		      t)))))))
     (ensure-same (queue-as-list (cl-transactional::value-of ttvar2))
 		 (list 'a 'b 'c 'd 'e 'f)
 		 :test #'equal))))
