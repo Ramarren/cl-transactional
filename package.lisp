8286910d1b45a5c3899ae35af7eea3461b9bcbf9
Ramarren <ramarren@gmail.com>
2008-03-21 17:00:00 +0100

    Export some symbols.

--- package.lisp
+++ package.lisp
@@ -1,5 +1,8 @@
 (defpackage :cl-transactional
   (:nicknames :stm)
+  (:export #:make-tvar #:get-tvar #:put-tvar
+	   #:with-retry-transaction #:with-orelse-transaction
+	   #:retry-transaction)
   (:use :cl :sb-thread :defclass-star :iterate)
   (:import-from :arnesi :curry :aif :it :with-unique-names :hash-table-keys :awhen))
 
