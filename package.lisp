60fdf17b53f37bdee5ad4da86231996f21e80a22
Ramarren <ramarren@gmail.com>
2009-09-13 15:46:59 +0200

    Export tvar-ref.

--- package.lisp
+++ package.lisp
@@ -2,7 +2,8 @@
   (:nicknames :stm)
   (:export #:make-tvar #:get-tvar #:put-tvar
 	   #:with-retry-transaction #:with-orelse-transaction
-	   #:retry-transaction)
+	   #:retry-transaction
+           #:tvar-ref)
   (:use :cl :iterate :alexandria))
 
 (in-package :cl-transactional)
